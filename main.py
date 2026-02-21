5e4f2e9e14636d440ea84c8f28e112836a65f888
MacPark15 <macpark015@gmail.com>
2026-02-21 16:10:33 -0600

    Add delta time and lock 60FPS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init() # Initialize pygame
+    clock = pygame.time.Clock() # Create a pygame clock object
+    delta_time = 0 # Delta Time in seconds. Updates every tick
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Set pygame display to variables set in constant.py
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}") # Prints pygame version
     print(f"Screen width: {SCREEN_WIDTH}") # Prints screen width
@@ -16,7 +18,7 @@ def main():
                 return
         screen.fill("black") # Fill screen black
         pygame.display.flip() # Update display
-
+        dt = (clock.tick(60)) / 1000 # Pause the game loop for 1/60th of a second. Runs game smoothly at 60FPS
 
 
 if __name__ == "__main__": # Calls main only if the file is run directly, not imported
